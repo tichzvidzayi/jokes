92fa111eecef067586dab6ea4bcbb775b2b35d31
Tich Zvidzayi <tzvidzie@gmail.com>
2024-10-12 11:56:05 +0200

    fetch_

--- jokes/src/components/Footer.tsx
+++ jokes/src/components/Footer.tsx
@@ -4,8 +4,9 @@ const Footer = () => {
   return (
     <>
       <footer className="bg-slate-800 absolute w-full h-40 bottom-0 ">
-        {" "}
-        Footer
+        <span className="text-sm absolute text-white bottom-5 p-16">
+          Created by: Tich Zvidzayi
+        </span>
       </footer>
     </>
   );
--- jokes/src/components/Slider.tsx
+++ jokes/src/components/Slider.tsx
@@ -1,7 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+// fetch joke
+
+const fetchJoke = async () => {
+  try {
+    const response = await fetch(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+
+    if (response.ok) {
+      const data = response.json();
+      console.log("Joke fetch was successful");
+      return data;
+    } else {
+      console.error("Fetch joke failed");
+    }
+  } catch (err) {
+    console.error("Error occured : ", err);
+  }
+};
+
 const Slider = () => {
+  const [joke, setJoke] = useState<{}>();
+
+  useEffect(() => {
+    const getJoke = async () => {
+      const res = await fetchJoke;
+      if (res) {
+        setJoke(res);
+      }
+    };
+    getJoke();
+  }, []);
+
   return (
     <>
-      <p> Hello world</p>
+      <p> Info</p>
+      <h1> Abc </h1>
     </>
   );
 };
